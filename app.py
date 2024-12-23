5e34f1053df8394cd79c466c12aafe149193b92c
KirsanowArtem <kirsanov.artem.d@gmail.com>
2024-12-23 09:06:47 +0200

    23.12.2024/09:07

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+
+def start_another_process():
+    subprocess.Popen(["python", "TgBot.py"])
+
+
+start_another_process()
+
+
+@app.route("/")
+def index():
+    return "zxc"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000)
